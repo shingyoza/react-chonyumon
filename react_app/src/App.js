ab1ffeee01ab6482e70d68d173477a1cd57b160c
shingyoza <shingyoza@gmail.com>
2020-02-12 00:19:35 +0900

    136
    Use properties

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -3,11 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.title = props.title;
+    this.message = props.message;
+  }
   render() {
     return <div>
-      <h1>React</h1>
-      <p>This is a sample component.</p>
-      <p>これはサンプルのコンポーネントです。</p>
+      <h1>{this.title}</h1>
+      <p>{this.message}</p>
     </div>
   };
 }
