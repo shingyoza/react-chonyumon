8fa117eb6fe960051bc7d70ac0960dd1d5cc08d0
shingyoza <shingyoza@gmail.com>
2020-02-11 23:51:25 +0900

    Change function to class

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div>
+class App extends Component {
+  render() {
+    return <div>
       <h1>React</h1>
       <p>This is a sample component.</p>
       <p>これはサンプルのコンポーネントです。</p>
     </div>
-  );
+  };
 }
 
 export default App;
